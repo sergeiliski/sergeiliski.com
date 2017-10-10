739804ad156d43c1190a746036ba8631b1b240ac
Sergei Liski <sergeiliski@gmail.com>
2017-10-10 13:59:26 +0100

    change stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,7 +177,6 @@
 
 							if (id in panels)
 								panels[id]._activate();
-
 						}
 
 					});
@@ -242,10 +241,29 @@
 
 	$('#contact').on('submit', function(e) {
 		e.preventDefault();
-		showEmailLoader();
-		setTimeout(function() {
-			showEmailText();
-		}, 2000);
+		$.ajax({
+			url: '//formspree.io/sergeiliski@gmail.com',
+			method: 'POST',
+			data: $('#contact').find('form').serialize(),
+			dataType: 'json',
+			beforeSend: function() {
+				removeEmailSuccess();
+				showEmailLoader();
+			},
+			success: function(data) {
+				setTimeout(function() {
+					showEmailText();
+					showEmailSuccess();
+					resetContactForm();
+				}, 1500);
+			},
+			error: function(err) {
+				setTimeout(function() {
+					showEmailText();
+					resetContactForm();
+				}, 1500);
+			}
+		});
 	});
 
 })(jQuery);
@@ -258,4 +276,22 @@ function showEmailLoader() {
 function showEmailText() {
 	$('#contact').find('.send-message').removeClass('hide');
 	$('#contact').find('.sk-circle').addClass('hide');
+}
+
+function removeEmailSuccess() {
+	$('.email-success').empty();
+}
+
+function showEmailSuccess() {
+	var success = '<svg id="successAnimation" class="animated" xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 70 70">' +
+		'<path id="successAnimationResult" fill="#D8D8D8" d="M35,60 C21.1928813,60 10,48.8071187 10,35 C10,21.1928813 21.1928813,10 35,10 C48.8071187,10 60,21.1928813 60,35 C60,48.8071187 48.8071187,60 35,60 Z M23.6332378,33.2260427 L22.3667622,34.7739573 L34.1433655,44.40936 L47.776114,27.6305926 L46.223886,26.3694074 L33.8566345,41.59064 L23.6332378,33.2260427 Z"/>' +
+		'<circle id="successAnimationCircle" cx="35" cy="35" r="24" stroke="#979797" stroke-width="2" stroke-linecap="round" fill="transparent"/>' +
+		'<polyline id="successAnimationCheck" stroke="#979797" stroke-width="2" points="23 34 34 43 47 27" fill="transparent"/>' +
+	'</svg>' +
+	'<span class="email-success-text">Thank you for contacting me!</span>';
+	$('.email-success').append(success);
+}
+
+function resetContactForm() {
+	$('#contact').find('form').find('input[type="text"], textarea').val('');
 }
