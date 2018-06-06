16ae3e00c7541a90c443b9868d05eb7dffd6176e
Sergei Liski <sergeiliski@gmail.com>
2018-06-06 20:59:47 +0100

    Revamp the whole thing

--- assets/js/helpers.js
+++
@@ -1,7 +0,0 @@
-// show text one letter at a time
-var showText = function (target, message, index, interval) {
-  if (index < message.length) {
-    $(target).append(message[index++]);
-    setTimeout(function () { showText(target, message, index, interval); }, interval);
-  }
-}
